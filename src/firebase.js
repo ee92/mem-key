9df452d5559a931e05d39be921bdd316d498dd9f
egor <eegor650@gmail.com>
2019-07-14 15:34:27 -0700

    move firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,7 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const config = {
-	apiKey: "AIzaSyBlHKEDVvcpqswaOelEDHySw2t24cYi5Ow",
-	authDomain: "mem-key.firebaseapp.com",
-	databaseURL: "https://mem-key.firebaseio.com",
-	projectId: "mem-key",
-	storageBucket: "",
-	messagingSenderId: "905763876608",
-	appId: "1:905763876608:web:e3cc67b558db0b6a"
-};
+import config from './config/firebase';
 
 firebase.initializeApp(config);
 
