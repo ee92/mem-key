edc8c372bf5203c8f74a524dd76c9d56da61094d
egor <eegor650@gmail.com>
2019-07-15 19:01:31 -0700

    clean

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,12 @@
 import * as firebase from "firebase/app";
-
 import "firebase/auth";
 import "firebase/firestore";
 
 import config from './config/firebase';
-
 firebase.initializeApp(config);
 
 const db = firebase.firestore();
 const auth = firebase.auth();
-
 const googleAuth = new firebase.auth.GoogleAuthProvider();
 
 export {
