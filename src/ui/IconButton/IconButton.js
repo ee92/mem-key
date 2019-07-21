4f5109bddd8cd587b7b063836e677af0a1f7a002
egor <eegor650@gmail.com>
2019-07-21 11:56:56 -0700

    prevent focus on mouse down

--- src/ui/IconButton/IconButton.js
+++ src/ui/IconButton/IconButton.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import styles from './IconButton.module.css';
+
+const ignoreFocus = (e) => e.preventDefault()
  
 const _IconButton = ({children, ...rest}) => (
-   <IconButton classes={{root: styles.root}} {...rest}>
+   <IconButton
+      onMouseDown={ignoreFocus}
+      classes={{root: styles.root}}
+      {...rest} 
+   >
       {children}
    </IconButton>
 )
